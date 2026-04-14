b71b38488f24a433c52fe787025b4eb97fa02564
Jainamfornax <jainam@fornaxhq.co>
2026-04-14 11:45:59 +0530

    Add files via upload

--- models/fact_sales_for_protein_company.sql
+++ models/fact_sales_for_protein_company.sql
@@ -1,15 +1,2 @@
-SELECT
-    *,
-    MIN(order_date) OVER (PARTITION BY customer_name) AS customer_first_order_date,
-
-    CASE
-        WHEN order_date = MIN(order_date) OVER (PARTITION BY customer_name)
-        THEN 1 ELSE 0
-    END AS is_new_customer,
-
-    CASE
-        WHEN order_date > MIN(order_date) OVER (PARTITION BY customer_name)
-        THEN 1 ELSE 0
-    END AS is_repeat_customer
-
+SELECT *
 FROM `linked-368910.lightdash_poc.fact_sales_for_protein_company`
